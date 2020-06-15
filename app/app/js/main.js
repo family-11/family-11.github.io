4affd9be5ef43944b11b55ea0be33014f0180a94
family-11 <66860348+family-11@users.noreply.github.com>
2020-06-15 12:17:28 +0300

    Add files via upload

--- app/app/js/main.js
+++ app/app/js/main.js
@@ -1,12 +1,14 @@
 $(function(){
 
     $('.slider__container').slick({
+        autoplay: false,
+        autoplaySpeed: 5000,
         dots: true,
-        infinite: false,
-        speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
         speed: 900,
+        infinite:false,
+        fade:true
       });
       window.onload = function () {
         document.body.classList.add('loaded_hiding');
@@ -15,6 +17,16 @@ $(function(){
           document.body.classList.remove('loaded_hiding');
         }, 500);
       }
+      
+
+
+
+
+
+     
+
+
+
 
       $("#tab").pagination({
         items: 3,
@@ -26,15 +38,17 @@ $(function(){
 
 
 
-       $('.header__burger-burger').click(function(event){
+       $('.header__burger-burger,.header__burger-partners-title').click(function(event){
             $('.header__burger-burger,.header__burger-hide').toggleClass('active');
             $('body').toggleClass('lock');
        });
        $('.nav__burger-burger').click(function(event){
         $('.nav__inner,.nav__burger-burger').toggleClass('active');
-   });
+        });
        $('.header__search').click(function(event){
         $('.header__search-input').toggleClass('active');
         });
+
+        
     
 });
